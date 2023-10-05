d2961b9e35e4373a4fb3dbee16e0ddc9b1b0b520
itsAvikrishna <147051608+itsAvikrishna@users.noreply.github.com>
2023-10-05 16:51:02 +0530

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,1 +1,25 @@
+import random
 
+print("PROJECT RPG ")
+print()
+
+print("Welcome to Random Password Generator")
+print()
+
+passwordchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$&%"
+
+nofpassword = int(input("Enter the number of password you want: "))
+passlen = int(input("Enter a length of password: "))
+
+print()
+print("Here are some random passwords ")
+print()
+
+for i in range(nofpassword):
+    password = ""
+    for j in range(passlen):
+        password = password + random.choice(passwordchar)
+    print(password)
+    
+print()
+print("These are some random password")
